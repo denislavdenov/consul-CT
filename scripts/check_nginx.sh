8479c0e47ea28cfd000e1c45cdec907514fb17d9
Denov <denov@Denovs-MacBook-Pro.local>
2019-01-28 15:20:25 +0200

    adding code

---
+++ scripts/check_nginx.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+
+# Check for nginx
+which nginx || {
+apt-get update -y
+apt-get install nginx -y
+}
+
+export HN=$(hostname)
+# Create script check
+
+cat << EOF > /usr/local/bin/check_wel.sh
+#!/usr/bin/env bash
+
+curl 127.0.0.1:80 | grep "Welcome to"
+EOF
+
+chmod +x /usr/local/bin/check_wel.sh
+
+# Register nginx in consul
+cat << EOF > /cclient/.consul.d/web.json
+{
+    "service": {
+        "name": "web",
+        "tags": ["${var1}"],
+        "port": 80
+    },
+    "checks": [
+        {
+            "id": "nginx_http_check",
+            "name": "Check nginx1",
+            "http": "http://127.0.0.1:80",
+            "tls_skip_verify": false,
+            "method": "GET",
+            "interval": "10s",
+            "timeout": "1s"
+        },
+        {
+            "id": "nginx_tcp_check",
+            "name": "TCP on port 80",
+            "tcp": "127.0.0.1:80",
+            "interval": "10s",
+            "timeout": "1s"
+        },
+        {
+            "id": "nginx_script_check",
+            "name": "Welcome check",
+            "args": ["/usr/local/bin/check_wel.sh", "-limit", "256MB"],
+            "interval": "10s",
+            "timeout": "1s"
+        }
+    ]
+}
+EOF
+
+
+#value=`curl -sL http://127.0.0.1:8500/v1/kv/nginx/${var1}?raw`
+#echo $value > /var/www/html/index.nginx-debian.html
+#echo $site > /var/www/html/index.nginx-debian.html
+
+#systemctl restart nginx.service
+consul-template -config /vagrant/config.hcl &
+
+sleep 1
+consul reload
---
+++ scripts/keyvalue.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/env bash
+
+########################
+# Using consul command #
+########################
+set -x
+# Adding key/values
+
+consul kv put nginx/config/connections 5
+consul kv put allowed_latency 100ms
+consul kv put admin_pass 1q2w3e4r
+consul kv put nginx/maxusers 25
+# Getting values from keys
+consul kv get nginx/config/connections
+consul kv get -detailed nginx/config/connections
+consul kv get allowed_latency
+consul kv get admin_pass
+consul kv get -recurse nginx/
+# Delete key/values
+consul kv delete allowed_latency
+consul kv delete -recurse nginx/
+
+########################
+#    Using REST API    #
+########################
+
+# Adding key/values
+curl \
+    --request PUT \
+    --data "nameNAME"  \
+    http://127.0.0.1:8500/v1/kv/website-name
+
+curl \
+    --request PUT \
+    --data "@/vagrant/denislav.json"  \
+    http://127.0.0.1:8500/v1/kv/denislav
+
+curl \
+    --request PUT \
+    --data '
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <title>Welcome to client-nginx1!</title>
+    <style>
+    body {
+        width: 35em;
+        margin: 0 auto;
+        font-family: Tahoma, Verdana, Arial, sans-serif;
+    }
+    </style>
+    </head>
+    <body>
+    <h1>Welcome to client-nginx1!</h1>
+    <p><em>Thank you for using client-nginx1.</em></p>
+    </body>
+    </html>'  \
+    http://127.0.0.1:8500/v1/kv/client-nginx1/site
+
+curl \
+    --request PUT \
+    --data '
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <title>Welcome to client-nginx2!</title>
+    <style>
+    body {
+        width: 35em;
+        margin: 0 auto;
+        font-family: Tahoma, Verdana, Arial, sans-serif;
+    }
+    </style>
+    </head>
+    <body>
+    <h1>Welcome to client-nginx2!</h1>
+    <p><em>Thank you for using client-nginx2.</em></p>
+    </body>
+    </html>'  \
+    http://127.0.0.1:8500/v1/kv/client-nginx2/site
+
+
+
+# Getting values from keys
+
+value=`curl -sL http://127.0.0.1:8500/v1/kv/website-name | jq '.[].Value' | tr -d '"' | base64 --decode`
+echo $value
+
+value=`curl -sL http://127.0.0.1:8500/v1/kv/denislav | jq '.[].Value' | tr -d '"' | base64 --decode`
+echo $value
+value=`curl -sL http://127.0.0.1:8500/v1/kv/client-nginx1/site?raw`
+echo $value
+curl -sL http://127.0.0.1:8500/v1/kv/denislav?raw
+# Deleting key/values
+curl \
+    --request DELETE \
+    http://127.0.0.1:8500/v1/kv/website-name
+
+set +x
